655ed899599ec645c49c8b1ecc8dda3677709451
MohamedMorabet <medmrbt5@email.com>
2024-04-22 12:36:53 +0100

    finish the styling of the navbar and footer

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,18 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home_text-container">
+          <h1 className="text-4xl font-extrabold">Car catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className="home_filters">
+          {/* the search bar will be there */}
+
+        </div>
+      </div>
     </main>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from "@/node_modules/next/image"
 import CustomButton from "./CustomButton"
 const Navbar = () => {
   return (
-        <header className="w-full z-10">
+        <header className="w-full absolute z-10">
             <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
                 <Link href="/" className="flex justify-center items-center">
                     <Image
